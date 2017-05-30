541f0465806af8270c39fdb8247419f82e1e6dd3
Nicolas St-Louis <stlouisn@hotmail.com>
2017-05-30 12:51:22 +0000

    docker_entrypoint.sh created online with Bitbucket

---
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Set timezone
+TZ=${TZ:-UTC}
+cp /usr/share/zoneinfo/$TZ /etc/localtime
+echo $TZ > /etc/timezone
+
+# Make sure volumes are mounted correctly
+if [ ! -d /var/lib/subsonic ]; then
+  printf "\nERROR: volume /var/lib/subsonic not mounted.\n" >&2
+  exit 1
+fi
+
+# Create symlink for transcoding
+if [ ! -e /var/lib/subsonic/transcode/ffmpeg ]; then
+  ln -s /usr/bin/ffmpeg /var/lib/subsonic/transcode/
+fi
+
+# Fix user and group ownerships
+chown -R subsonic:subsonic /var/lib/subsonic
+chown -R subsonic:subsonic /music /podcasts /playlists /videos
+
+# Change workdir
+cd /usr/lib/subsonic
+
+# Start subsonic in console mode
+exec su-exec subsonic \
+  /usr/bin/java \
+    -Dsubsonic.home=/var/lib/subsonic \
+    -Dsubsonic.host=0.0.0.0 \
+    "$@" \
+    -Dsubsonic.contextPath=/ \
+    -Dsubsonic.defaultMusicFolder=/music \
+    -Dsubsonic.defaultPodcastFolder=/podcasts \
+    -Dsubsonic.defaultPlaylistFolder=/playlists \
+    -Djava.awt.headless=true \
+    -jar /usr/lib/subsonic/subsonic-booter-jar-with-dependencies.jar
