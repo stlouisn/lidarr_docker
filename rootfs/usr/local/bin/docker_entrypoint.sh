88205cb935e6e3c39d81c942e4f031dba82ab9c6
stlouisn <stlouisn@hotmail.com>
2017-10-10 19:03:18 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -5,6 +5,19 @@ TZ=${TZ:-UTC}
 cp /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
 
+# Copy default subsonic.properties file
+#if [ ! -e /var/lib/sonarr/? ]; then
+#    cp /etc/sonarr/? /var/lib/sonarr/.
+#fi
+
+# Fix user and group ownerships
+#umask 022
+chown -R 10000:10000 /var/lib/sonarr || exit 1
+
+# Change workdir
+#cd /opt/NzbDrone
+
 # Start sonarr in console mode
 exec gosu sonarr \
-    mono --debug NzbDrone.exe -nobrowser -data=/var/lib/sonarr
+    /usr/bin/mono --debug \
+    /opt/NzbDrone/NzbDrone.exe -nobrowser -data=/var/lib/sonarr
