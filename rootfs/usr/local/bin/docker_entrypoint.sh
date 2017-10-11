94c96efa544eb8c260593bb291046049d51002af
stlouisn <stlouisn@hotmail.com>
2017-10-10 21:34:50 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -5,20 +5,12 @@ TZ=${TZ:-UTC}
 cp /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
 
-# Copy default sonarr config file
-#if [ ! -e /var/lib/sonarr/ ]; then
-#    cp /etc/sonarr/? /var/lib/sonarr/.
-#fi
-
 # Delete pid if it exists
 [[ -e /var/lib/sonarr/nzbdrone.pid ]] && rm -f /var/lib/sonarr/nzbdrone.pid
 
 # Fix user and group ownerships
 #umask 022
 chown -R 10000:10000 /var/lib/sonarr || exit 1
-
-# Change workdir
-#cd /opt/NzbDrone
 
 # Start sonarr in console mode
 exec gosu sonarr \
