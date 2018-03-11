c41f72a73452ab70e4990611b9c4104f8ee4db9b
stlouisn <stlouisn@hotmail.com>
2018-03-11 15:59:47 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -4,7 +4,6 @@
 [[ -e /var/lib/sonarr/nzbdrone.pid ]] && rm -f /var/lib/sonarr/nzbdrone.pid
 
 # Fix user and group ownerships
-#umask 022
 chown -R 10000:10000 /var/lib/sonarr || exit 1
 
 # Start sonarr in console mode
