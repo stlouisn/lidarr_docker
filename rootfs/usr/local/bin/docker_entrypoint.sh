bfd914fd5f56b385106c1685fd9729a9f5c66309
stlouisn <stlouisn@hotmail.com>
2018-03-11 18:06:23 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Make sure volume '/var/lib/sonarr' is writeable
+if [[ ! -d /var/lib/sonarr ]]; then
+    echo -e "\nError: volume '/var/lib/sonarr' not mounted.\n" >&2
+    exit 1
+elif [[ `mount | grep '/var/lib/sonarr' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
+    echo -e "\nError: volume '/var/lib/sonarr' is readonly.\n" >&2
+    exit 1
+fi
+
 # Fix user and group ownerships
-chown -R sonarr:sonarr /config || ( echo -e "\nError: volume '/config' not mounted correctly.\n"; exit 1 ) || exit 1
+chown -R sonarr:sonarr /var/lib/sonarr
 
 # Delete pid if it exists
 [[ -e /var/lib/sonarr/nzbdrone.pid ]] && rm -f /var/lib/sonarr/nzbdrone.pid
@@ -9,4 +18,4 @@ chown -R sonarr:sonarr /config || ( echo -e "\nError: volume '/config' not mount
 # Start sonarr in console mode
 exec gosu sonarr \
     /usr/bin/mono --debug \
-    /opt/NzbDrone/NzbDrone.exe -nobrowser -data=/config
+    /opt/NzbDrone/NzbDrone.exe -nobrowser -data=/var/lib/sonarr
