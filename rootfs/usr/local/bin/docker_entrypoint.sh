0faaf6b3eefc8f65009aec0b6e5d7305330e164c
stlouisn <stlouisn@hotmail.com>
2018-03-11 16:15:59 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+# Fix user and group ownerships
+chown -R sonarr:sonarr /config || ( echo -e "\nError: volume '/config' not mounted correctly.\n"; exit 1 ) || exit 1
+
 # Delete pid if it exists
 [[ -e /var/lib/sonarr/nzbdrone.pid ]] && rm -f /var/lib/sonarr/nzbdrone.pid
-
-# Fix user and group ownerships
-chown -R 10000:10000 /var/lib/sonarr || exit 1
 
 # Start sonarr in console mode
 exec gosu sonarr \
     /usr/bin/mono --debug \
-    /opt/NzbDrone/NzbDrone.exe -nobrowser -data=/var/lib/sonarr
+    /opt/NzbDrone/NzbDrone.exe -nobrowser -data=/config
