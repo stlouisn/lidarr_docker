3bed7a4f4721916ed982b64d07535eb3949f026d
stlouisn <stlouisn@hotmail.com>
2018-04-17 14:07:34 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -2,24 +2,24 @@
 
 #=========================================================================================
 
-# Make sure volume '/var/lib/sonarr' is mounted and writeable
-if [[ ! -d /var/lib/sonarr ]]; then
-    echo -e "\nError: volume '/var/lib/sonarr' is not mounted.\n" >&2
+# Make sure volume '/var/lib/lidarr' is mounted and writeable
+if [[ ! -d /var/lib/lidarr ]]; then
+    echo -e "\nError: volume '/var/lib/lidarr' is not mounted.\n" >&2
     exit 1
-elif [[ `mount | grep '/var/lib/sonarr' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nError: volume '/var/lib/sonarr' is readonly.\n" >&2
+elif [[ `mount | grep '/var/lib/lidarr' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
+    echo -e "\nError: volume '/var/lib/lidarr' is readonly.\n" >&2
     exit 1
 fi
 
-# Fix user and group ownerships for '/var/lib/sonarr'
-chown -R sonarr:sonarr /var/lib/sonarr
+# Fix user and group ownerships for '/var/lib/lidarr'
+chown -R lidarr:lidarr /var/lib/lidarr
 
 # Delete pid if it exists
-[[ -e /var/lib/sonarr/nzbdrone.pid ]] && rm -f /var/lib/sonarr/nzbdrone.pid
+#[[ -e /var/lib/lidarr/nzbdrone.pid ]] && rm -f /var/lib/lidarr/nzbdrone.pid
 
 #=========================================================================================
 
-# Start sonarr in console mode
-exec gosu sonarr \
+# Start lidarr in console mode
+exec gosu lidarr \
     /usr/bin/mono --debug \
-    /opt/NzbDrone/NzbDrone.exe -nobrowser -data=/var/lib/sonarr
+    /opt/Lidarr/Lidarr.exe -nobrowser -data=/var/lib/lidarr
