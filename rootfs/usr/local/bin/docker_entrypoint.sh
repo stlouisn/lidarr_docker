92d2776720cbfdbeecbe189033cb9889e55fc9b5
stlouisn <stlouisn@hotmail.com>
2019-01-21 19:07:00 -0400

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -2,24 +2,15 @@
 
 #=========================================================================================
 
-# Make sure volume '/var/lib/lidarr' is mounted and writeable
-if [[ ! -d /var/lib/lidarr ]]; then
-    echo -e "\nError: volume '/var/lib/lidarr' is not mounted.\n" >&2
-    exit 1
-elif [[ `mount | grep '/var/lib/lidarr' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nError: volume '/var/lib/lidarr' is readonly.\n" >&2
-    exit 1
-fi
-
-# Fix user and group ownerships for '/var/lib/lidarr'
-chown -R lidarr:lidarr /var/lib/lidarr
+# Fix user and group ownerships for '/config'
+chown -R lidarr:lidarr /config
 
 # Delete pid if it exists
-[[ -e /var/lib/lidarr/lidarr.pid ]] && rm -f /var/lib/lidarr/lidarr.pid
+[[ -e /config/lidarr.pid ]] && rm -f /config/lidarr.pid
 
 #=========================================================================================
 
 # Start lidarr in console mode
 exec gosu lidarr \
     /usr/bin/mono --debug \
-    /opt/Lidarr/Lidarr.exe -nobrowser -data=/var/lib/lidarr
+    /opt/Lidarr/Lidarr.exe -nobrowser -data=/config
